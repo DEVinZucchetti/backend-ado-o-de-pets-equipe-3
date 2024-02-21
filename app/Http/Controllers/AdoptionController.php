b92aef7ad6a004682763ccf2a09531f45f1a1722
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2024-02-21 10:20:28 -0300

    feat: finaliza exercício

--- app/Http/Controllers/AdoptionController.php
+++ app/Http/Controllers/AdoptionController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\SendWelcomePet;
+use App\Mail\SendDocuments;
+use App\Models\Adoption;
 use App\Models\Client;
+use App\Models\File;
 use App\Models\People;
 use App\Models\Pet;
+use App\Models\SolicitationDocument;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
 use Symfony\Component\HttpFoundation\Response;
 
 class AdoptionController extends Controller
@@ -19,7 +26,7 @@ class AdoptionController extends Controller
     {
         try {
 
-            // pegar os dados que foram enviados via query params
+            // pega dados que foram enviados via query params
             $filters = $request->query();
 
             // inicializa uma query
@@ -27,21 +34,13 @@ class AdoptionController extends Controller
                 ->select(
                     'id',
                     'pets.name as pet_name',
-                    'pets.race_id',
-                    'pets.specie_id',
-                    'pets.size as size',
-                    'pets.weight as weight',
                     'pets.age as age'
                 )
                 #->with('race') // traz todas as colunas
-                ->with(['race' => function ($query) {
-                    $query->select('name', 'id');
-                }])
-                ->with('specie')
                 ->where('client_id', null);
 
 
-            // verifica se filtro
+            // verifica se há filtro
             if ($request->has('name') && !empty($filters['name'])) {
                 $pets->where('name', 'ilike', '%' . $filters['name'] . '%');
             }
@@ -77,5 +76,74 @@ class AdoptionController extends Controller
         if (!$pet) return $this->error('Dado não encontrado', Response::HTTP_NOT_FOUND);
 
         return $pet;
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $data = $request->all(); // pega body
+
+            $request->validate([
+                'name' => 'string|required|max:255',
+                'contact' => 'string|required|max:20',
+                'email' => 'string|required',
+                'cpf' => 'string|required',
+                'observations' => 'string|required',
+                'pet_id' => 'integer|required',
+            ]); // validação dos dados
+
+            $adoption = Adoption::create([...$data, 'status' => 'PENDENTE']);
+            return $adoption;
+
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function getAdoptions()
+    {
+        $adoptions = Adoption::query()->with('pet')->get();
+        return $adoptions;
+    }
+
+    public function approve(Request $request)
+    {
+
+        try {
+            $data = $request->all();
+
+            $request->validate([
+                'adoption_id' => 'integer|required',
+            ]);
+
+            $adoption = Adoption::find($data['adoption_id']);
+
+            if (!$adoption)  return $this->error('Dado não encontrado', Response::HTTP_NOT_FOUND);
+
+            $adoption->update(['status' => 'APROVADO']);
+            $adoption->save();
+
+            $people = People::create([
+                'name' => $adoption->name,
+                'email' => $adoption->email,
+                'cpf' => $adoption->cpf,
+                'contact' => $adoption->contact,
+            ]);
+
+            $client = Client::create([
+                'people_id' => $people->id,
+                'bonus' => true
+            ]);
+
+            $pet = Pet::find($adoption->pet_id); // Busca id do pet e vincula ao cliente
+            $pet->update(['client_id' => $client->id]);
+            $pet->save();
+
+            return $client;
+
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
     }
 }
