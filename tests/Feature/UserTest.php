f03ad54619b58f2755f43e73de61ba639e64f12d
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2024-02-23 00:35:05 -0500

    feat:verificacao dos permisos de admin

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -24,13 +24,49 @@ class UserTest extends TestCase
             'password' => env("DEFAULT_PASSWORD"),
         ]);
 
+        //Verificar se o status code esta como esperado
         $response->assertStatus(201);
 
-
-        /*
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-        */
+        $response->assertJson([
+            "message" => "Autorizado",
+            "status"=> 201,
+            "data"=> [
+                 "token"=> true,
+                 "permissions"=> true,
+            ]
+        ]);      
     }
+
+    //testo permisos de usuarios
+    public function test_user_admin_permissions_load_correct()
+    {
+
+        $this->seed(Profiles::class);
+        $this->seed(InitialUser::class);
+
+        $response = $this->post('/api/login',[
+            'email' => env("DEFAULT_EMAIL"),
+            'password' => env("DEFAULT_PASSWORD"),
+        ]);
+
+        $response->assertStatus(201);
+
+        $response->assertJson([
+            "data"=> [
+                "permissions"=>[
+                'create-races',
+                'get-races',
+                'create-species',
+                'get-species',
+                'delete-species',
+                'create-pets',
+                'get-pets',
+                'delete-pets',
+                'create-profissionals',
+                'get-profissionals']
+            ],
+        ]);      
+    }
+
+
 }
